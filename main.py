50e0965ed1614f5418444d14445383f0bbe059ae
cat-loverrr <usachevaisabella@gmail.com>
2025-06-06 12:21:30 +1000

    Another commit

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ print(df.head())
 
 # Show the column names
 print(df.columns)
+
+print(df['Sport'].value_counts().head())
+
+print(df['Sex'].value_counts())
+
+print(df.describe())
+
+print(df['NOC'].nunique())
+print(df['NOC'].unique())
