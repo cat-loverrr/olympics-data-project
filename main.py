336fb8295d6df01f9eaf7592ad3a7c589423e2cb
cat-loverrr <usachevaisabella@gmail.com>
2025-06-10 11:06:55 +1000

    Filtering, Sorting and Grouping

--- main.py
+++ main.py
@@ -17,3 +17,24 @@ print(df.describe())
 
 print(df['NOC'].nunique())
 print(df['NOC'].unique())
+
+# Filter for female athletes only
+female_athletes = df[df['Sex'] == 'F']
+print(female_athletes.head())
+
+# Filter for athletes older than 35
+older_athletes = df[df['Age'] > 35]
+print(older_athletes[['Name', 'Age', 'Sport']].head())
+
+
+# Athletes from Australia in Swimming
+combo_filter = df[(df['Team'] == 'Australia') & (df['Sport'] == 'Swimming')]
+print(combo_filter[['Name', 'Team', 'Sport']].head())
+
+# Sort by height
+sorted_by_height = df.sort_values(by='Height', ascending=False)
+print(sorted_by_height[['Name', 'Height', 'Sport']].head())
+
+# Sort by weight
+sorted_by_weight = df.sort_values(by='Weight', ascending=False)
+print(sorted_by_weight[['Name', 'Weight', 'Sport']].head())
